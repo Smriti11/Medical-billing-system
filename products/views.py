8794f14d060e8ede3aeb5fdc8a6dd6643a7c76c7
Smriti Thapa <smriti.thapa66@gmail.com>
2018-07-11 01:01:24 +0545

    tables with update del and search buttons created

--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-<<<<<<< HEAD
 from .models import Products
 # Create your views here.
 def products(request):
@@ -7,13 +6,3 @@ def products(request):
     'products':Products.objects.all()
     }
     return render(request,'products.html',context)
-=======
-from django_tables2 import RequestConfig
-from .tables import ProductTable
-from .models import Products
-# Create your views here.
-def products(request):
-    table = ProductTable(Products.objects.all())
-    RequestConfig(request).configure(table)
-    return render(request,'products.html',{'table':table})
->>>>>>> 3dab086088074a8709f3e30114718087c3d24e78
--- purchase/views.py
+++ purchase/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from .models import Purchase
 
@@ -7,15 +6,3 @@ def purchase(request):
     'purchase':Purchase.objects.all()
     }
     return render(request,'purchase.html',context)
-=======
-
-from django.shortcuts import render
-from django_tables2 import RequestConfig
-from .models import Purchase
-from .tables import PurchaseTable
-
-def purchase(request):
-    table = PurchaseTable(Purchase.objects.all())
-    RequestConfig(request).configure(table)
-    return render(request, 'purchase.html', {'table': table})
->>>>>>> 3dab086088074a8709f3e30114718087c3d24e78
