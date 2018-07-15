2bc3b6e97db59931656333559911efde5f75f355
Rohan Pradhan <rohanpradhan88@gmail.com>
2018-07-15 13:20:49 +0545

    added functions to update and delete button

--- purchase/urls.py
+++ purchase/urls.py
@@ -6,7 +6,7 @@ from .import views
 urlpatterns = [
         url(r'^purchase/$',views.purchase,name='purchase'),
         url(r'^purchase/(?P<id>[0-9]+)/update/$',views.purchase_update,name='purchase_update'),
-        url(r'^purchase/(?P<id>[0-9]+)/delete',views.purchase_delete,name='purchase_delete'),
+        url(r'^purchase/(?P<id>[0-9]+)/delete/$',views.purchase_delete,name='purchase_delete'),
 
 
         ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Purchase
 from django.http import HttpResponse
+from .forms import AddPurchaseForm
 from django.shortcuts import redirect
 
 
@@ -10,15 +11,20 @@ def purchase(request):
     }
     return render(request,'purchase.html',context)
 
-def purchase_update(request):
-    context={
-    'purchase_update':Purchase.objects.get(id=id)
+def purchase_update(request,id):
+    purchase= Purchase.objects.get(pk=id)
+    form = AddPurchaseForm(request.POST or None,instance=purchase)
+    if form.is_valid():
+        form.save()
+        return redirect('purchase')
+    context = {
+    'form':form,
     }
     return render(request,'purchase_update.html',context)
 
 def purchase_delete(request,id):
     try:
-        purchase_delete= get_Purchase_or_404(Model, pk=purchase_id)
+        purchase= Purchase.objects.get(pk=id)
         purchase.delete()
         return redirect('purchase')
     except:
